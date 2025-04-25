81022a9cfdd7ac94558c6e921e3d86546ac41efc
Ahmad Reza Adrian <ahmadadrian324@gmail.com>
2025-04-25 23:12:47 +0700

    feat: create user (admin only) management (#26)
    * refactor: refactor API routes for improved error handling and code clarity
    
    * feat: implement user (admin only) management endpoints for GET, POST, PUT, and DELETE operations

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -40,6 +40,7 @@ export async function POST(request: Request) {
       { status: 201 }
     );
   } catch (error) {
+    console.log(error);
     return NextResponse.json(
       { message: "Internal Server Error" },
       { status: 500 }
