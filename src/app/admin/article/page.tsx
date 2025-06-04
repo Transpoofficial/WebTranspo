59329bbc42f3c1e3c89663ff0d2800efeb816454
FathanAlfariel <103872242+FathanAlfariel@users.noreply.github.com>
2025-06-05 06:08:49 +0700

    feat: adding a menu to the admin dashboard (#60)
    * feat: create landing and auth pages
    
    * feat: create a layout for the admin page

---
+++ src/app/admin/article/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React from "react";
+// import VehicleTypeTable from "./components/vehicle-type-table";
+import { Button } from "@/components/ui/button";
+// import VehicleTypeCreateDialog from "./components/vehicle-type-create-dialog";
+
+const Article = () => {
+  // const [isVehicleTypeCreateDialogOpen, setIsVehicleTypeCreateDialogOpen] =
+  //   useState<boolean>(false);
+
+  // const handleOpenVehicleTypeCreateDialog = (): void => {
+  //   setIsVehicleTypeCreateDialogOpen(true);
+  // };
+
+  return (
+    <>
+      <h2 className="text-3xl font-bold tracking-tight first:mt-0">
+        Paket wisata
+      </h2>
+
+      <div className="flex justify-end mt-4 mb-1">
+        <Button
+          // onClick={handleOpenVehicleTypeCreateDialog}
+          className="cursor-pointer"
+        >
+          Tambah paket wisata
+        </Button>
+      </div>
+
+      {/* Vehicle type dialog */}
+      {/* <VehicleTypeCreateDialog
+        isVehicleTypeCreateDialogOpen={isVehicleTypeCreateDialogOpen}
+        setIsVehicleTypeCreateDialogOpen={setIsVehicleTypeCreateDialogOpen}
+      />
+
+      <VehicleTypeTable /> */}
+    </>
+  );
+};
+
+export default Article;
