04cd88ef92a21b0ecb0409c33fe3d1e14da1ff06
FathanAlfariel <103872242+FathanAlfariel@users.noreply.github.com>
2025-06-08 00:05:04 +0700

    feat: remove the tour package from the home page and implementing the API on login and signup (#64)
    * feat: create landing and auth pages
    
    * feat: create a layout for the admin page
    
    * feat: implement add destination functionality with google maps API
    
    * feat: integrate Google Maps API for route calculation and display
    
    * chore: update .env.example file
    
    * feat: add integration google maps and add destinations
    
    * style: improve styling
    
    * feat: add order to landing page and improve some UI
    
    * style: improve style for some element
    
    * refactor: delete the phone input in sign up and add an image
    
    * refactor: remove phone number from signup schema and update related components
    
    * refactor: remove hardcoded phone number from user creation in signup route
    
    * refactor: delete unused files in components
    
    * refactor: refactor Map2 component and add custom styles for step indicators
    
    * feat: remove the tour package from the home page and implementing the API on login and signup
    
    * feat: remove the tour package from the home page and implementing the API on login and signup
    
    ---------
    
    Co-authored-by: rezadrian01 <ahmadadrian324@gmail.com>

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -6,6 +6,7 @@ declare module "next-auth" {
       id: string;
       email: string;
       fullName: string;
+      role: Role;
     } & DefaultSession["user"];
   }
 
@@ -13,6 +14,7 @@ declare module "next-auth" {
     id: string;
     email: string;
     fullName: string;
+    role: Role;
   }
 }
 
@@ -21,5 +23,6 @@ declare module "next-auth/jwt" {
     id: string;
     email: string;
     fullName: string;
+    role: Role;
   }
 }
