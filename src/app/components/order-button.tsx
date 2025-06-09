dc63c8a861379bedbb49acb7ab78d56327a852b9
rezadrian01 <ahmadadrian324@gmail.com>
2025-06-09 09:28:51 +0700

    feat: implement customer dashboard API and enhance order button functionality

--- src/app/components/order-button.tsx
+++ src/app/components/order-button.tsx
@@ -1,15 +1,23 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 import React, { FC } from "react";
 
 interface OrderButtonProps {
   content: string;
   type: "TRANSPORT" | "TOUR_PACKAGE";
+  isDashboard?: boolean;
+  textColor?: string;
 }
 
-const OrderButton: FC<OrderButtonProps> = ({ content, type }) => {
+const OrderButton: FC<OrderButtonProps> = ({
+  content,
+  type,
+  isDashboard,
+  textColor,
+}) => {
   const router = useRouter();
   const handleOrderClick = () => {
     const encodedContent = encodeURIComponent(content);
@@ -20,14 +28,31 @@ const OrderButton: FC<OrderButtonProps> = ({ content, type }) => {
     }
   };
   return (
-    <Button
-      size="lg"
-      className="py-8 px-8 text-xl w-min text-[#0897B1] bg-white hover:bg-gray-100 shadow-lg"
-      onClick={handleOrderClick}
-      data-testid="order-button"
-    >
-      Pesan sekarang
-    </Button>
+    <>
+      {!isDashboard && (
+        <Button
+          size="lg"
+          className="py-8 px-8 text-xl w-min text-transpo-primary bg-white hover:bg-gray-100 shadow-lg"
+          onClick={handleOrderClick}
+          data-testid="order-button"
+        >
+          Pesan sekarang
+        </Button>
+      )}
+      {isDashboard && (
+        <Button
+          size="lg"
+          className={cn(
+            "py-2 px-4 text-xl w-min bg-white hover:bg-gray-100 shadow-lg",
+            { [textColor as string]: textColor }
+          )}
+          onClick={handleOrderClick}
+          data-testid="order-button-dashboard"
+        >
+          Pesan Sekarang
+        </Button>
+      )}
+    </>
   );
 };
 
