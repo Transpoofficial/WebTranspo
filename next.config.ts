32eb7c7e7aa108869dd4456151eb8a42bd350067
Ahmad Reza Adrian <ahmadadrian324@gmail.com>
2025-07-28 20:16:13 +0700

    chore: update env file configuration (#135)

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['atofjdakcdpmetccupvn.supabase.co'],
+    // domains: ['atofjdakcdpmetccupvn.supabase.co'],
+    domains: [`${process.env.SUPABASE_HOST}.supabase.co`],
   },
 };
 
